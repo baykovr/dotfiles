2d5c9ec32840ee2cf612e425381aaa15efd091c1
Robert Baykov <baykovr@gmail.com>
2023-08-21 15:51:07 -0700

    Add home-manager flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    description = "/home/baykovr";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+        };
+    };
+    outputs = {nixpkgs, home-manager, ...}: {
+        defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+        homeConfigurations = {
+            "baykovr" = home-manager.lib.homeManagerConfiguration {
+                pkgs = import nixpkgs { system = "x86_64-linux"; };
+                modules = [ ./home.nix ]; # Defined later
+            };
+        };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,6 @@
+{pkgs, ...}: {
+    home.username = "baykovr";
+    home.homeDirectory = "/home/baykovr";
+    home.stateVersion = "23.05"; 
+    programs.home-manager.enable = true;
+}
