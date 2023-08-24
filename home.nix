5a91994de5565076d2cf638fe7e4dbfd33ec725b
Robert Baykov <baykovr@gmail.com>
2023-08-24 14:36:26 -0700

    Add neovim configurations

--- home.nix
+++ home.nix
@@ -3,11 +3,36 @@
     home.homeDirectory = "/home/baykovr";
     home.stateVersion = "23.05"; 
     programs.home-manager.enable = true;
+    
     home.packages = [
         pkgs.nixpkgs-fmt
         pkgs.most
-        pkgs.neovim
         pkgs.fuse
     ];
 
+    programs.neovim = {
+      enable = true;
+	    defaultEditor = true;
+	    extraConfig = ''
+	      set nocompatible
+	      set termguicolors
+	      syntax on
+	      filetype plugin indent on
+
+	      set number
+	      set ttyfast
+	      set cursorline
+
+	      set autoindent expandtab tabstop=2 shiftwidth=2
+
+        colorscheme gruvbox-material
+        set background=light
+	    '';
+
+	    plugins = with pkgs.vimPlugins; [
+        nvim-treesitter.withAllGrammars
+        gruvbox-material
+        nvim-tree-lua
+	    ];
+    };
 }
