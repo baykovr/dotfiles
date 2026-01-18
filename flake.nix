4af2ece6543176822de7274887ca98525e9a722f
Robert Baykov <baykovr@gmail.com>
2026-01-17 16:13:42 -0800

    add: keifu

--- flake.nix
+++ flake.nix
@@ -11,8 +11,12 @@
           url = "github:peterldowns/nix-search-cli";
           #inputs.follows = nixpkgs;
         };
+        rust-overlay = {
+          url = "github:oxalica/rust-overlay";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-    outputs = {self, nixpkgs, home-manager, nix-search-cli, flake-utils, ...}:
+    outputs = {self, nixpkgs, home-manager, nix-search-cli, flake-utils, rust-overlay, ...}:
         let
           system = "aarch64-darwin";
           pkgs = nixpkgs.legacyPackages.${system};
@@ -24,6 +28,7 @@
             "baykovr" = home-manager.lib.homeManagerConfiguration {
               pkgs = import nixpkgs {
                 system = "${system}";
+                overlays = [ rust-overlay.overlays.default ];
                 config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
                   "terraform"
                   "code"
